5d92c6e30430e5fb4ce22ca82b497d02d802236f
Christoph Kappestein <k42b3.x@gmail.com>
2015-07-12 12:17:10 +0200

    fix coding style

--- src/Fusio/Template/Filter/Json.php
+++ src/Fusio/Template/Filter/Json.php
@@ -2,19 +2,19 @@
 /*
  * Fusio
  * A web-application to create dynamically RESTful APIs
- * 
+ *
  * Copyright (C) 2015 Christoph Kappestein <k42b3.x@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -42,7 +42,7 @@ class Json
 
         if (is_array($value)) {
             $value = new Object($value);
-        } else if($value instanceof \stdClass) {
+        } elseif ($value instanceof \stdClass) {
             $value = new Object((array) $value);
         }
 
--- tests/Fusio/Action/HttpRequestTest.php
+++ tests/Fusio/Action/HttpRequestTest.php
@@ -2,19 +2,19 @@
 /*
  * Fusio
  * A web-application to create dynamically RESTful APIs
- * 
+ *
  * Copyright (C) 2015 Christoph Kappestein <k42b3.x@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -43,7 +43,7 @@ class HttpRequestTest extends \PHPUnit_Framework_TestCase
 
         $http->expects($this->once())
             ->method('request')
-            ->with($this->callback(function($request){
+            ->with($this->callback(function ($request) {
                 /** @var \PSX\Http\RequestInterface $request */
                 $this->assertInstanceOf('PSX\Http\RequestInterface', $request);
                 $this->assertJsonStringEqualsJsonString('{"foo":"bar"}', (string) $request->getBody());
