ef3c88bac22a1c74f5064f15d29eeebf67674461
Christoph Kappestein <k42b3.x@gmail.com>
2015-07-18 20:45:41 +0200

    add documentation

---
+++ public/documentation/config.js
@@ -0,0 +1,14 @@
+
+var evid_config = {
+
+	/**
+	 * Path to the PSX documentation endpoint
+	 */
+	url: '/projects/fusio/public/index.php/doc',
+
+	/**
+	 * Paths which should be excluded from the documentation
+	 */
+	exclude: ['/backend/(.*)', '/authorization/(.*)']
+
+};
