049bd8d081a61861f82ca01477ad6c5deda5a4b7
Christoph Kappestein <k42b3.x@gmail.com>
2015-08-15 10:06:32 +0200

    increase version

--- src/Fusio/Base.php
+++ src/Fusio/Base.php
@@ -30,7 +30,7 @@ namespace Fusio;
  */
 class Base
 {
-    const VERSION = '0.1.3';
+    const VERSION = '0.1.4';
 
     public static function getVersion()
     {
--- src/Fusio/Database/Installer.php
+++ src/Fusio/Database/Installer.php
@@ -94,6 +94,7 @@ class Installer
     public function getUpgradePath()
     {
         return [
+            '0.1.4',
             '0.1.3',
             '0.1.2',
             '0.1.1',
