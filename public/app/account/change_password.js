6a0fec22b36d00cf33f60c43ca5bebcac5ce8051
Christoph Kappestein <k42b3.x@gmail.com>
2015-09-10 21:46:58 +0200

    add account

---
+++ public/app/account/change_password.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('fusioApp.account', ['ngRoute'])
+
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/account/change_password', {
+        templateUrl: 'app/account/change_password.html',
+        controller: 'ChangePasswordCtrl'
+    });
+}])
+
+.controller('ChangePasswordCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.account = {
+        oldPassword: "",
+        newPassword: "",
+        verifyPassword: ""
+    };
+
+    $scope.updatePassword = function(){
+        $http.put(fusio_url + 'backend/account/change_password', $scope.account)
+            .success(function(data){
+                $scope.response = data;
+            })
+            .error(function(data){
+                $scope.response = data;
+            });
+    };
+
+    $scope.closeResponse = function(){
+        $scope.response = null;
+    };
+
+}]);
