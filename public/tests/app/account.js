a44d681cae8a7b6a6ac7fed052362d263d111f2d
Christoph Kappestein <k42b3.x@gmail.com>
2015-09-12 22:18:25 +0200

    fix account test

--- public/tests/app/account.js
+++ public/tests/app/account.js
@@ -4,6 +4,8 @@ describe('Account tests', function() {
   it('Change password', function() {
     browser.get('#/account/change_password');
 
+    var EC = protractor.ExpectedConditions;
+
     element(by.model('account.oldPassword')).sendKeys('3632465b3b5d1b8b4b8e56f74600da00cea92baf');
     element(by.model('account.newPassword')).sendKeys('3632465b3b5d1b8b4b8e56f74600da00cea92baf');
     element(by.model('account.verifyPassword')).sendKeys('3632465b3b5d1b8b4b8e56f74600da00cea92baf');
