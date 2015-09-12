0b255f4d3a38b936d670259e2ca59cd84e63552f
Christoph Kappestein <k42b3.x@gmail.com>
2015-09-12 22:09:55 +0200

    add account change password test

---
+++ public/tests/app/account.js
@@ -0,0 +1,18 @@
+
+describe('Account tests', function() {
+
+  it('Change password', function() {
+    browser.get('#/account/change_password');
+
+    element(by.model('account.oldPassword')).sendKeys('3632465b3b5d1b8b4b8e56f74600da00cea92baf');
+    element(by.model('account.newPassword')).sendKeys('3632465b3b5d1b8b4b8e56f74600da00cea92baf');
+    element(by.model('account.verifyPassword')).sendKeys('3632465b3b5d1b8b4b8e56f74600da00cea92baf');
+
+    $('button.btn-primary').click();
+
+    browser.wait(EC.visibilityOf($('div.alert-success')), 5000);
+
+    expect($('div.alert-success > div').getText()).toEqual('Password successful changed');
+  });
+
+});
--- public/tests/conf.js
+++ public/tests/conf.js
@@ -1,6 +1,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
+    'app/account.js',
     'app/action.js',
     'app/app.js',
     'app/connection.js',
