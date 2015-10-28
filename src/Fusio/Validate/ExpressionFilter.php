3033eac2468f57be8d0d1cdfeaf0091e546b312a
Christoph Kappestein <k42b3.x@gmail.com>
2015-10-28 21:40:12 +0100

    add validator

---
+++ src/Fusio/Validate/ExpressionFilter.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Validate;
+
+use PSX\Cache;
+use PSX\FilterAbstract;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\ParsedExpression;
+use Symfony\Component\ExpressionLanguage\ParserCache\ParserCacheInterface;
+
+/**
+ * ExpressionFilter
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class ExpressionFilter extends FilterAbstract implements ParserCacheInterface
+{
+    protected $container;
+    protected $cache;
+    protected $expression;
+    protected $message;
+
+    public function __construct(ServiceContainer $container, Cache $cache, $expression, $message)
+    {
+        $this->container  = $container;
+        $this->cache      = $cache;
+        $this->expression = $expression;
+        $this->message    = $message;
+    }
+
+    public function apply($value)
+    {
+        $language = new ExpressionLanguage($this);
+        $values   = array();
+
+        foreach ($this->container as $name => $service) {
+            $values[$name] = $service;
+        }
+
+        $values['value'] = $value;
+
+        return $language->evaluate($this->expression, $values);
+    }
+
+    public function getErrorMessage()
+    {
+        return $this->message;
+    }
+
+    public function save($key, ParsedExpression $expression)
+    {
+        $item = $this->cache->getItem(md5($key));
+        $item->set($expression);
+
+        $this->cache->save($item);
+    }
+
+    public function fetch($key)
+    {
+        $item = $this->cache->getItem(md5($key));
+
+        return $item->isHit() ? $item->get() : null;
+    }
+
+}
---
+++ src/Fusio/Validate/Util.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Validate;
+
+use Doctrine\DBAL\Connection;
+use RuntimeException;
+
+/**
+ * Util
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class Util
+{
+    public static function rowExists($connection, $table, $column, $value)
+    {
+        if (!preg_match('/^[A-z0-9\_]{1,64}$/', $table)) {
+            throw new RuntimeException('Table name "' . $table . '" contains invalid characters');
+        }
+
+        if (!preg_match('/^[A-z0-9\_]{1,64}$/', $column)) {
+            throw new RuntimeException('Column name "' . $column . '" contains invalid characters');
+        }
+
+        if ($connection instanceof Connection) {
+            $queryBuilder = $connection->createQueryBuilder();
+
+            $queryBuilder
+                ->select($connection->getDatabasePlatform()->getCountExpression($column))
+                ->from($table)
+                ->where($column . ' = ?');
+
+            $count = (int) $connection->fetchColumn($queryBuilder->getSQL(), [$value]);
+
+            return $count > 0;
+        } else {
+            // @TODO handle other connection types i.e. MongoDB
+        }
+
+        return false;
+    }
+}
