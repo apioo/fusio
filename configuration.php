2909239f817e8de8083aa98188c44c8b7dfa097d
Christoph Kappestein <k42b3.x@gmail.com>
2015-11-21 22:06:59 +0100

    add log configuration

--- configuration.php
+++ configuration.php
@@ -2,36 +2,41 @@
 
 return array(
 
-	// The url to the psx public folder (i.e. http://127.0.0.1/psx/public or 
-	// http://localhost.com)
-	'psx_url'                 => 'http://127.0.0.1/projects/fusio/public',
+    // The url to the psx public folder (i.e. http://127.0.0.1/psx/public or 
+    // http://localhost.com)
+    'psx_url'                 => 'http://127.0.0.1/projects/fusio/public',
 
-	// The input path 'index.php/' or '' if you use mod_rewrite
-	'psx_dispatch'            => 'index.php/',
+    // The input path 'index.php/' or '' if you use mod_rewrite
+    'psx_dispatch'            => 'index.php/',
 
-	// The default timezone
-	'psx_timezone'            => 'UTC',
+    // The default timezone
+    'psx_timezone'            => 'UTC',
 
-	// Whether PSX runs in debug mode or not. If not error reporting is set to 0
-	'psx_debug'               => true,
+    // Whether PSX runs in debug mode or not. If not error reporting is set to 0
+    'psx_debug'               => true,
 
-	// Your SQL connections
-	'psx_sql_host'            => 'localhost',
-	'psx_sql_user'            => 'root',
-	'psx_sql_pw'              => '',
-	'psx_sql_db'              => 'fusio',
+    // Log settings, the handler is one of: stream, logcaster, void, system
+    'psx_log_level'           => \Monolog\Logger::ERROR,
+    'psx_log_handler'         => 'system',
+    'psx_log_uri'             => null,
 
-	// Path to the cache folder
-	'psx_path_cache'          => __DIR__ . '/cache',
+    // Your SQL connections
+    'psx_sql_host'            => 'localhost',
+    'psx_sql_user'            => 'root',
+    'psx_sql_pw'              => '',
+    'psx_sql_db'              => 'fusio',
 
-	// Path to the library folder
-	'psx_path_library'        => __DIR__ . '/src',
+    // Path to the cache folder
+    'psx_path_cache'          => __DIR__ . '/cache',
 
-	// Class name of the error controller
-	//'psx_error_controller'    => null,
+    // Path to the library folder
+    'psx_path_library'        => __DIR__ . '/src',
 
-	// If you only want to change the appearance of the error page you can 
-	// specify a custom template
-	//'psx_error_template'      => null,
+    // Class name of the error controller
+    //'psx_error_controller'    => null,
+
+    // If you only want to change the appearance of the error page you can 
+    // specify a custom template
+    //'psx_error_template'      => null,
 
 );
