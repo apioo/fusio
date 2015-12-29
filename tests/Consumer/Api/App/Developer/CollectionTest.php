6d1963b5e9743c7f3884ab0b2558653789a21506
Christoph Kappestein <k42b3.x@gmail.com>
2015-12-29 13:47:10 +0100

    add consumer api test

---
+++ tests/Consumer/Api/App/Developer/CollectionTest.php
@@ -0,0 +1,152 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Consumer\Api\App\Developer;
+
+use Fusio\Impl\Fixture;
+use Fusio\Impl\Backend\Table\App;
+use PSX\Test\ControllerDbTestCase;
+use PSX\Test\Environment;
+
+/**
+ * CollectionTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class CollectionTest extends ControllerDbTestCase
+{
+    public function getDataSet()
+    {
+        return Fixture::getDataSet();
+    }
+
+    public function testGet()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/developer', 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ));
+
+        $body = (string) $response->getBody();
+        $body = preg_replace('/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/m', '[datetime]', $body);
+        $body = preg_replace('/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/m', '[app_key]', $body);
+
+        $expect = <<<'JSON'
+{
+    "totalItems": 2,
+    "startIndex": 0,
+    "entry": [
+        {
+            "id": 2,
+            "userId": 1,
+            "status": 1,
+            "name": "Consumer",
+            "appKey": "[app_key]",
+            "date": "[datetime]"
+        },
+        {
+            "id": 1,
+            "userId": 1,
+            "status": 1,
+            "name": "Backend",
+            "appKey": "[app_key]",
+            "date": "[datetime]"
+        }
+    ]
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+    }
+
+    public function testPost()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/developer', 'POST', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ), json_encode([
+            'status' => 3, // status and userID are ignored so it doesnt matter
+            'userId' => 2, // if they are available
+            'name'   => 'Foo',
+            'url'    => 'http://google.com',
+            'scopes' => ['foo', 'bar']
+        ]));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<'JSON'
+{
+    "success": true,
+    "message": "App successful created"
+}
+JSON;
+
+        $this->assertEquals(201, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+
+        // check database
+        $sql = Environment::getService('connection')->createQueryBuilder()
+            ->select('id', 'status', 'userId', 'name', 'url')
+            ->from('fusio_app')
+            ->orderBy('id', 'DESC')
+            ->setFirstResult(0)
+            ->setMaxResults(1)
+            ->getSQL();
+
+        $row = Environment::getService('connection')->fetchAssoc($sql);
+
+        $this->assertEquals(6, $row['id']);
+        $this->assertEquals(App::STATUS_ACTIVE, $row['status']);
+        $this->assertEquals(1, $row['userId']);
+        $this->assertEquals('Foo', $row['name']);
+        $this->assertEquals('http://google.com', $row['url']);
+    }
+
+    public function testPut()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/developer', 'PUT', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ), json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(405, $response->getStatusCode(), $body);
+    }
+
+    public function testDelete()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/developer', 'DELETE', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ), json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(405, $response->getStatusCode(), $body);
+    }
+}
---
+++ tests/Consumer/Api/App/Grant/EntityTest.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Consumer\Api\App\Grant;
+
+use Fusio\Impl\Fixture;
+use Fusio\Impl\Backend\Table\App;
+use PSX\Test\ControllerDbTestCase;
+use PSX\Test\Environment;
+
+/**
+ * EntityTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class EntityTest extends ControllerDbTestCase
+{
+    public function getDataSet()
+    {
+        return Fixture::getDataSet();
+    }
+
+    public function testGet()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/grant/1', 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(405, $response->getStatusCode(), $body);
+    }
+
+    public function testPost()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/grant/1', 'POST', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ), json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(405, $response->getStatusCode(), $body);
+    }
+
+    public function testPut()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/grant/1', 'PUT', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ), json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(405, $response->getStatusCode(), $body);
+    }
+
+    public function testDelete()
+    {
+        $response = $this->sendRequest('http://127.0.0.1/consumer/app/grant/1', 'DELETE', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b8f6f61bd22b440a3e4be2b7491066682bfcde611dbefa1b15d2e7f6522d77e2'
+        ));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(204, $response->getStatusCode(), $body);
+    }
+}
