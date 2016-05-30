0c364f39e08b82d65108590e477b14eb5527895c
Christoph Kappestein <k42b3.x@gmail.com>
2016-05-30 07:30:25 +0200

    use 0.3 schema

--- public/server.php
+++ public/server.php
@@ -81,7 +81,7 @@ if (isset($_SERVER['argv']) && in_array('--warmup', $_SERVER['argv'])) {
 
     // create schema
     $fromSchema = $connection->getSchemaManager()->createSchema();
-    $version    = new Fusio\Impl\Database\Version\Version020();
+    $version    = new Fusio\Impl\Database\Version\Version030();
     $toSchema   = $version->getSchema();
     Fusio\Impl\TestSchema::appendSchema($toSchema);
 
