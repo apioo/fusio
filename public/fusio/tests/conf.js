827ee0bf0b58f098c7270cef94c2ac5cafce07da
Christoph Kappestein <k42b3.x@gmail.com>
2016-11-19 23:18:56 +0100

    use index.htm file for php server

--- public/fusio/tests/conf.js
+++ public/fusio/tests/conf.js
@@ -17,13 +17,13 @@ exports.config = {
     'app/user.js',
     'app/import.js'
   ],
-  baseUrl: 'http://127.0.0.1:8008/fusio',
+  baseUrl: 'http://127.0.0.1:8008/fusio/index.htm',
   capabilities: {
     browserName: 'firefox'
   },
   onPrepare: function() {
     // login
-    browser.driver.get('http://127.0.0.1:8008/fusio');
+    browser.driver.get('http://127.0.0.1:8008/fusio/index.htm');
     browser.driver.findElement(by.id('username')).sendKeys('Developer');
     browser.driver.findElement(by.id('password')).sendKeys('qf2vX10Ec3wFZHx0K1eL');
     browser.driver.findElement(by.css('button[type="submit"]')).click();
