e09811d8df8b45fd33ddbe39c63143de10ce81eb
Christoph Kappestein <k42b3.x@gmail.com>
2016-12-29 18:14:28 +0100

    driver sleep

--- public/fusio/tests/conf.js
+++ public/fusio/tests/conf.js
@@ -24,11 +24,7 @@ exports.config = {
   onPrepare: function() {
     // login
     browser.driver.get('http://127.0.0.1:8008/fusio/index.htm#/login');
-
-    browser.wait(function(){
-      return browser.driver.findElement(by.id('username')).isPresent();
-    }, 5000);
-
+    browser.driver.sleep(2000);
     browser.driver.findElement(by.id('username')).sendKeys('Developer');
     browser.driver.findElement(by.id('password')).sendKeys('qf2vX10Ec3wFZHx0K1eL');
     browser.driver.findElement(by.css('button[type="submit"]')).click();
