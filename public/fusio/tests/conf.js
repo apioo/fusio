9f98095514018498852d3cdd44e9280142bafc14
Christoph Kappestein <k42b3.x@gmail.com>
2016-12-30 12:53:44 +0100

    revert conf

--- public/fusio/tests/conf.js
+++ public/fusio/tests/conf.js
@@ -25,25 +25,14 @@ exports.config = {
     // login
     browser.driver.get('http://127.0.0.1:8008/fusio/index.htm#/login');
 
-    console.log('28');
+    browser.driver.findElement(by.id('username')).sendKeys('Developer');
+    browser.driver.findElement(by.id('password')).sendKeys('qf2vX10Ec3wFZHx0K1eL');
+    browser.driver.findElement(by.css('button[type="submit"]')).click();
 
-    return browser.driver.sleep(4000).then(function(){
-      console.log('31');
-      browser.driver.findElement(by.css('body')).then(function(el){
-        el.getAttribute('innerHTML').then(function(value){
-          console.log(value);
-        });
+    return browser.driver.wait(function() {
+      return browser.driver.getCurrentUrl().then(function(url) {
+        return /#\/dashboard/.test(url);
       });
-
-      browser.driver.findElement(by.id('username')).sendKeys('Developer');
-      browser.driver.findElement(by.id('password')).sendKeys('qf2vX10Ec3wFZHx0K1eL');
-      browser.driver.findElement(by.css('button[type="submit"]')).click();
-
-      return browser.driver.wait(function() {
-        return browser.driver.getCurrentUrl().then(function(url) {
-          return /#\/dashboard/.test(url);
-        });
-      }, 10000);
-    });
+    }, 10000);
   }
 };
