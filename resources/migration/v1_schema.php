a6fbe6b397274cea6bd36880f9d96123bdec6c8b
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-11-09 20:57:30 +0100

    insert demo data on migration

--- resources/migration/v1_schema.php
+++ resources/migration/v1_schema.php
@@ -11,3 +11,9 @@ $todoTable->addColumn('status', 'integer', ['default' => 1]);
 $todoTable->addColumn('title', 'string', ['length' => 64]);
 $todoTable->addColumn('insertDate', 'datetime');
 $todoTable->setPrimaryKey(['id']);
+
+// insert demo data
+$date = new \DateTime();
+for ($i = 1; $i < 32; $i++) {
+    $migration->addSql('INSERT INTO app_todo (status, title, insertDate) VALUES (?, ?, ?)', [1, 'Task ' . $i, $date->format('Y-m-d H:i:s')]);
+}
