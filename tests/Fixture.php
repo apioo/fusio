4fe41cb772a50136644d72a3671d26c3de851047
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-11-17 20:02:53 +0100

    use data bag

--- tests/Fixture.php
+++ tests/Fixture.php
@@ -22,6 +22,7 @@
 namespace App\Tests;
 
 use Fusio\Impl\Connection\Native;
+use Fusio\Impl\Migrations\DataBag;
 use Fusio\Impl\Migrations\NewInstallation;
 
 /**
@@ -36,34 +37,29 @@ class Fixture
     protected static $dataSet;
 
     /**
-     * Returns the default Fusio system inserts, through this it is i.e.
+     * Appends the default Fusio system inserts, through this it is i.e.
      * possible to add test users or apps which are required for your API. The
      * test token needs the required scopes to access your endpoints
      * 
      * @return array
      * @throws \Exception
      */
-    public static function getSystemInserts()
+    public static function append(DataBag $dataBag): void
     {
         $expire = new \DateTime();
         $expire->add(new \DateInterval('P1M'));
 
         $scopes = ['todo'];
 
-        return [
-            'fusio_scope' => [
-                ['name' => 'testing', 'description' => 'Test scope'],
-            ],
-            'fusio_app_scope' => [
-                ['app_id' => 1, 'scope_id' => 4],
-            ],
-            'fusio_app_token' => [
-                ['app_id' => 1, 'user_id' => 1, 'status' => 1, 'token' => 'da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf', 'scope' => implode(',', $scopes), 'ip' => '127.0.0.1', 'expire' => $expire->format('Y-m-d H:i:s'), 'date' => '2015-06-25 22:49:09'],
-            ],
-            'fusio_user_scope' => [
-                ['user_id' => 1, 'scope_id' => 4],
-            ]
-        ];
+        $dataBag->addScope('default', 'testing', 'Test scope');
+        $dataBag->addAppScope('Backend', 'testing');
+        $dataBag->addAppToken('Backend', 'Administrator', 'da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf', '', implode(',', $scopes), $expire->format('Y-m-d H:i:s'), '2015-06-25 22:49:09');
+        $dataBag->addUserScope('Administrator', 'testing');
+
+        $inserts = self::getDemoInserts();
+        foreach ($inserts as $tableName => $rows) {
+            $dataBag->addTable($tableName, $rows);
+        }
     }
 
     /**
@@ -86,25 +82,21 @@ class Fixture
             'app_todo' => $result
         ];
     }
-    
+
     public static function getFixture()
     {
         if (self::$dataSet !== null) {
             return self::$dataSet;
         }
 
-        $installInserts = NewInstallation::getData();
+        $dataBag = NewInstallation::getData();
 
         // replace System connection class
-        $installInserts['fusio_connection'][0]['class'] = Native::class;
+        $dataBag->replace('fusio_connection', 'System', 'class', Native::class);
 
-        $dataSet = array_merge_recursive(
-            $installInserts,
-            self::getSystemInserts(),
-            self::getDemoInserts()
-        );
+        self::append($dataBag);
 
-        return self::$dataSet = $dataSet;
+        return self::$dataSet = $dataBag;
     }
 }
 
