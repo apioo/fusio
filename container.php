39b9a643c0a33a95f8d0a09aa9a61ad5294513c1
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-07-09 11:45:45 +0200

    add cli container file

--- container.php
+++ container.php
@@ -4,6 +4,7 @@ return \PSX\Framework\Dependency\ContainerBuilder::build(
     __DIR__,
     true,
     __DIR__ . '/vendor/psx/framework/resources/container.php',
+    __DIR__ . '/vendor/fusio/cli/resources/container.php',
     __DIR__ . '/vendor/fusio/impl/resources/container.php',
     __DIR__ . '/resources/container.php',
     ...\Fusio\Impl\Adapter\AdapterFinder::getFiles(__DIR__ . '/provider.php')
