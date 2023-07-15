b8fc138424b66f6495b706751bea2ba1c8df8058
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-07-15 21:42:05 +0200

    use closure and dont explicit set debug status

--- container.php
+++ container.php
@@ -2,10 +2,13 @@
 
 return \PSX\Framework\Dependency\ContainerBuilder::build(
     __DIR__,
-    true,
-    __DIR__ . '/vendor/psx/framework/resources/container.php',
-    __DIR__ . '/vendor/fusio/cli/resources/container.php',
-    __DIR__ . '/vendor/fusio/impl/resources/container.php',
-    __DIR__ . '/resources/container.php',
-    ...\Fusio\Impl\Adapter\AdapterFinder::getFiles(__DIR__ . '/provider.php')
+    null,
+    fn () => [
+        __DIR__ . '/vendor/psx/framework/resources/container.php',
+        __DIR__ . '/vendor/fusio/cli/resources/container.php',
+        __DIR__ . '/vendor/fusio/engine/resources/container.php',
+        __DIR__ . '/vendor/fusio/impl/resources/container.php',
+        __DIR__ . '/resources/container.php',
+        ...\Fusio\Impl\Adapter\AdapterFinder::getFiles(__DIR__ . '/provider.php'),
+    ],
 );
