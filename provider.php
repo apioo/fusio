7191a27a989948d2a36737827cc59104fc12a6bf
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-04-21 19:11:07 +0200

    update deps and add new provider

--- provider.php
+++ provider.php
@@ -1,15 +1,19 @@
 <?php
 
 return [
+    \Fusio\Adapter\Amqp\Adapter::class,
+    \Fusio\Adapter\Beanstalk\Adapter::class,
     \Fusio\Adapter\Cli\Adapter::class,
     \Fusio\Adapter\Fcgi\Adapter::class,
     \Fusio\Adapter\File\Adapter::class,
     \Fusio\Adapter\GraphQL\Adapter::class,
     \Fusio\Adapter\Http\Adapter::class,
     \Fusio\Adapter\Php\Adapter::class,
+    \Fusio\Adapter\Redis\Adapter::class,
     \Fusio\Adapter\Smtp\Adapter::class,
     \Fusio\Adapter\Soap\Adapter::class,
     \Fusio\Adapter\Sql\Adapter::class,
+    \Fusio\Adapter\Stripe\Adapter::class,
     \Fusio\Adapter\Util\Adapter::class,
     \Fusio\Adapter\Worker\Adapter::class,
 ];
