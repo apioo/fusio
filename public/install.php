12176dd8aa4fd2de56cba3f4b5350c815755c1dd
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-05-14 19:35:05 +0200

    add title and lang

--- public/install.php
+++ public/install.php
@@ -286,8 +286,9 @@ function alert(string $level, string $message): void
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <title>Fusio Installer</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <style type="text/css">
