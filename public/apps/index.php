9646701fd4e1e06ace59da86fcf1070aadd74632
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-07-12 11:17:36 +0200

    add meta

--- public/apps/index.php
+++ public/apps/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE>
 <html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Fusio - Apps</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
